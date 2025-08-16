3aa52717cfafb08bdb3cdc0a1f55d7f67e61c6b4
Md Intekhab Alam <intekhab8928@gmail.com>
2025-08-16 18:57:53 +0530

    Fixed the internal pages

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -5,7 +5,7 @@ import propertyData from '@/utils/PropertyData';
 function CardList() {
   return (
     <div className="max-w-6xl mx-auto my-12 px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid lg:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {propertyData.map((property, index) => (
           <Card key={index} {...property} />
         ))}
